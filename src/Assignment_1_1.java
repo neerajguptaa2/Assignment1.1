f415cdadf54b1e0c035eef58c24c3203cb187e41
neerajguptaa2 <neerajguptaa@ymail.com>
2016-11-23 19:03:43 +0530

    Assignment1.1
    Assignment1.1

---
+++ src/Assignment_1_1.java
@@ -0,0 +1,19 @@
+
+public class Assignment_1_1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		int c = a+b;
+		
+		//first method
+		int i;
+		for (i=1; i<=b; i++)
+		{
+			a++;			
+		}
+		System.out.println(a);
+	}
+
+}
